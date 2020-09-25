1726c86b715234d234db53e57834e88d97f357e5
Tonny Ouma <tonouma@amazon.com>
2020-09-25 09:59:45 -0500

    remove chalice app

--- textract-pipeline-app/app.py
+++
@@ -1,8 +0,0 @@
-from chalice import Chalice
-
-app = Chalice(app_name='textract-pipeline-app')
-
-@app.lambda_function(name='pdf2image')
-def pdf2image_lambda_function(event, context):
-    # Anything you want here.
-    return {}
